31ec506d831125c7556748d7c3238bf3f9393c67
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-23 02:42:55 +0700

    Add getter setter

--- src/main/java/com/rainhard/jdbc/entity/Roles.java
+++ src/main/java/com/rainhard/jdbc/entity/Roles.java
@@ -26,6 +26,23 @@ public class Roles {
     @LastModifiedDate
     @Column("updatedAt")
     private LocalDateTime updatedAt;
+
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getRoleName() {
+        return roleName;
+    }
+
+    public void setRoleName(String roleName) {
+        this.roleName = roleName;
+    }
 }
 
 
