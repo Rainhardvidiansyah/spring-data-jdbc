08926935fde6aa36fc318e31968f41b33b0348cd
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-26 02:12:52 +0700

    Create UserRole Repository

---
+++ src/main/java/com/rainhard/jdbc/repository/UserRolesRepository.java
@@ -0,0 +1,12 @@
+package com.rainhard.jdbc.repository;
+
+
+import com.rainhard.jdbc.entity.UserRoles;
+import org.springframework.data.repository.CrudRepository;
+
+public interface UserRolesRepository extends CrudRepository<UserRoles, Long> {
+
+
+
+
+}
