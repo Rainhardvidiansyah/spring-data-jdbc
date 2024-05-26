9552d5f9cee9bd7f79b01ca459dd62f1e38a6906
Rainhardvidiansyah <rainhardvidians@gmail.com>
2024-05-27 02:55:50 +0700

    Add some methods

--- src/main/java/com/rainhard/jdbc/service/UsersService.java
+++ src/main/java/com/rainhard/jdbc/service/UsersService.java
@@ -1,46 +1,53 @@
 package com.rainhard.jdbc.service;
 
-import com.rainhard.jdbc.dto.UsersDto;
 import com.rainhard.jdbc.entity.Users;
 import com.rainhard.jdbc.repository.UsersRepository;
-import org.modelmapper.ModelMapper;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
 
 @Service
+@Transactional
 public class UsersService {
 
     private static final Logger log = LoggerFactory.getLogger(UsersService.class);
     @Autowired
-    private ModelMapper modelMapper;
-    @Autowired
     private UsersRepository usersRepository;
 
-    public Users registration(UsersDto usersDto) {
-        var user = convertToEntity(usersDto);
-        log.info("User DTO Service class:{}", usersDto.toString());
-        return this.usersRepository.save(user);
+    @Autowired
+    private UserRolesService userRolesService;
+
+    @Transactional
+    public Users registration(Users users) {
+        return usersRepository.save(users);
     }
 
     public List<Users> findAllUsers(){
         return this.usersRepository.findAllUsers();
     }
 
-    private Users convertToEntity(UsersDto usersDto){
-        Users users = modelMapper.map(usersDto, Users.class);
-        return users;
+    public boolean checkUserEmail(String email){
+        var user = this.usersRepository.findByEmail(email);
+        return user.isPresent();
     }
 
-    private UsersDto convertToDataTransfer(Users users){
-        if(users != null){
-            return modelMapper.map(users, UsersDto.class);
-        }else {
-            throw new IllegalStateException();
-        }
+    public boolean checkUserId(Long id){
+        var user = this.usersRepository.findById(id);
+        return user.isPresent();
     }
+
+    public void setRoles(Long userId){
+        userRolesService.addUserRole(userId);
+    }
+
+    public int updateUser(Long id, String username){
+        return this.usersRepository.updateUser(id, username);
+    }
+
 }
